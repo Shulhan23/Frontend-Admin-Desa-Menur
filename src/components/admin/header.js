d6675c60a0aa33a72786c611b19eda98361a5c4e
Shulhan23 <129780285+Shulhan23@users.noreply.github.com>
2025-07-21 18:53:01 +0700

    Update header.js

--- src/components/admin/header.js
+++ src/components/admin/header.js
@@ -1,10 +1,17 @@
 // src/components/admin/Header.js
 'use client'
+import { Menu } from 'lucide-react'
 
-export default function Header({ onLogout }) {
+export default function Header({ onLogout, toggleSidebar }) {
   return (
     <header className="bg-white border-b shadow-sm px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-semibold text-green-700">Desa Menur - Admin</h1>
+      <div className="flex items-center gap-4">
+        {/* Tombol burger untuk semua ukuran layar */}
+        <button onClick={toggleSidebar} className="text-green-700 md:hidden block">
+          <Menu size={24} />
+        </button>
+        <h1 className="text-xl font-semibold text-green-700">Desa Menur - Admin</h1>
+      </div>
       <button
         onClick={onLogout}
         className="text-sm text-red-600 hover:text-red-800 hover:underline transition"
@@ -14,3 +21,4 @@ export default function Header({ onLogout }) {
     </header>
   )
 }
+
