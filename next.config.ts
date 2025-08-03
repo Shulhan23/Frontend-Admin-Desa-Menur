3c3f323e0b7041f2444671e0ba2fc1c6408d5442
Shulhan23 <shulhanaziz@gmail.com>
2025-08-03 23:39:47 +0700

    Memberikan .ENV

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.ts
+import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined in .env");
+}
+
+const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/laravel-api/:path*',
-        destination: 'http://localhost:8000/:path*',
+        source: "/laravel-api/:path*",
+        destination: `${API_URL}/:path*`,
       },
       {
-        source: '/storage/:path*',
-        destination: 'http://localhost:8000/storage/:path*',
+        source: "/storage/:path*",
+        destination: `${API_URL}/storage/:path*`,
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
